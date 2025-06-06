6265de906ee9850aa01b2061d8d371ed158959ef
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-06-07 04:10:47 +0530

    episode 3 done

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { jsx } from "react/jsx-runtime";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "Heading 1"),
-        React.createElement("h2", {}, "Heading 2"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "Heading 1"),
-        React.createElement("h2", {}, "Heading 2"),
-    ]),
-]);
+// React createElement => ReactElement - js Object => HTML Element(render)
+
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React🚀");
+
+// jsx is not HTML in js, but its a HTML-like syntax
+// jsx (transpiled before it reaches the browser) - PARCEL - Babel
+// jsx => Babel transpiles it to React.createElement => React createElement => ReactElement - js Object => HTML Element(render)
+
+//React Element
+const jsxheading = (
+  <h1 className="head" tabIndex="5">
+    Namaste JSX🚀
+  </h1>
+);
+
+const Title = () => {
+  return (
+    <h1 className="head" tabIndex="5">
+      Namaste React🚀
+    </h1>
+  );
+};
+
+//React Component
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <Title />
+      {jsxheading}
+      <h1 className="head" tabIndex="5">
+        React Functional Component
+      </h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);

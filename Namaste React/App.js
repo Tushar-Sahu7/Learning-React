28011c1ab03e4cb7fea9db124d11f08f16a2dcbe
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-06-07 23:05:21 +0530

    maintained folder structure

--- Namaste React/App.js
+++ Namaste React/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { jsx } from "react/jsx-runtime";
 
 // React createElement => ReactElement - js Object => HTML Element(render)
 

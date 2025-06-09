c996e7e0a24430947b42cf73904c0f448eac5fbd
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-06-09 15:05:14 +0530

    Assignment 04 folder added

---
+++ Assignment 04/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <>
+      <h1>Namaste Food App</h1>
+      <p>
+        Edit <code>src/App.jsx</code> to get started!
+      </p>
+    </>
+  );
+}
